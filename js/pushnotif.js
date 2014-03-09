2cf94d65f0a45a472dff02af56f510fa137bdb29
john.e <johnreriksson@gmail.com>
2014-03-09 14:46:10 +0100

    added push notifications to frontend

---
+++ js/pushnotif.js
@@ -0,0 +1,30 @@
+var pushNotification = window.plugins.pushNotification;
+pushNotification.register(push.successHandler, push.errorHandler,{"senderID":"764406565523","ecb":"push.onNotificationGCM"});
+
+var push = {
+	successHandler: function(result){
+		alert('Callback: '+result);
+	},
+	errorHandler: function(error){
+		alert(error);
+	},
+	onNotificationGCM: function(e){
+		switch (e.event) {
+			case 'registered' :
+				if(e.regid > 0){
+					alert('Registered: ' + e.regid);
+
+				}
+				break;
+			case 'message' :
+				alert(e.message);
+				break;
+			case 'error' : 
+				alert(e.msg);
+				break;
+			default :
+				alert('Something pushy happened');
+				break;
+		}
+	}
+}
