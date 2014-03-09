902b5c9d6f83cf3050048a3fba3e16b81e725650
john.e <johnreriksson@gmail.com>
2014-03-09 22:02:59 +0100

    fixin' all the bugs

--- js/pushnotif.js
+++ js/pushnotif.js
@@ -15,8 +15,8 @@ var push = {
 		switch(e.event)
 		{
 			case 'registered' :
-				alert(e.regid);
-				console.log('RegID: '+e.regid);
+				var regid = e.regid;
+				console.log('RegID: '+regid);
 				var url = "https://api.pushbots.com/deviceToken";
 				$.ajax({
 					method: 'PUT',
@@ -25,8 +25,8 @@ var push = {
 						req.setRequestHeader('X-PUSHBOTS-APPID', '531ca2661d0ab1f27c8b457b');
 						req.setRequestHeader('content-Type', 'application/json');
 					},
-					token: e.regid,
-					platform: '1',
+					"token": regid,
+					"platform": '1',
 					success: function(data){
 						console.log(data);
 					}
