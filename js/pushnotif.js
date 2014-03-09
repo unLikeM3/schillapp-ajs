b0739550caf9f54d9d35ee2746ac8fe6f6b12eec
john.e <johnreriksson@gmail.com>
2014-03-09 20:39:41 +0100

    debug added

--- js/pushnotif.js
+++ js/pushnotif.js
@@ -3,6 +3,29 @@
  */
 var pushNotification;
 
+var push = {
+	successHandler = function(result){
+		alert('Registered: '+result);
+	},
+	errorHandler = function(err){
+		alert('Error: '+err);
+	},
+	onNotif = function(e){
+		switch(e.event)
+		{
+			case 'registered' :
+				alert(e.regid);
+				break;
+			case 'message' :
+				alert(e.payload.message);
+				break;
+			case 'error' :
+				alert(e.msg);
+				break;
+		}
+	}
+}
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
@@ -12,29 +35,5 @@ function onDeviceReady(){
 			'senderID': '764406565523',
 			'ecb': 'push.onNotif'
 		});
-	}
-
-	var push = {
-		successHandler = function(result){
-			alert('Registered: '+result);
-		},
-		errorHandler = function(err){
-			alert('Error: '+err);
-		},
-		onNotif = function(e){
-			switch(e.event)
-			{
-				case 'registered' :
-					alert(e.regid);
-					break;
-				case 'message' :
-					alert(e.payload.message);
-					break;
-				case 'error' :
-					alert(e.msg);
-					break;
-			}
-		}
 	}	
 }
-
